088fdf79b31aae7194c05150d48f1000c66ab5b4
jonkastonka <jonkastonka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-03 10:20:26 +0000

    tagging version 1.02
    git-svn-id: https://plugins.svn.wordpress.org/my-youtube-playlist/trunk@201364 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myyoutubeplaylist/myYoutubePlaylist.js
+++ myyoutubeplaylist/myYoutubePlaylist.js
@@ -14,12 +14,14 @@ function myYoutubePlaylist_cy(ytSrc,containerId,type) {
 
 //a function to load flashmovies. it also prevents ie from adding the ugly border
 function myYoutubePlaylist_cf(FlashVars,movie,width,height,type) {
-	myYoutubePlaylist_cfStr = '<obj' + 'ect width="' + width + '" height="' + height + '">';
-	myYoutubePlaylist_cfStr += '<param name="movie" value="http://www.youtube.com/'+type+'/'+ movie +'&hl=en&fs=1"></param>';
+	myYoutubePlaylist_cfStr = '<obj' + 'ect width="' + width + '" height="' + height + '" data="' + 'http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1' + '" type="application/x-shockwave-flash">';
+	myYoutubePlaylist_cfStr += '<!--[if lte IE 6]>';
+	myYoutubePlaylist_cfStr += '<param name="movie" value="http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1"></param>';
 	myYoutubePlaylist_cfStr += '<param name="allowFullScreen" value="true"></param>';
 	myYoutubePlaylist_cfStr += '<param name="allowscriptaccess" value="always"></param>';
 	myYoutubePlaylist_cfStr += '<param name="FlashVars" value="'+ FlashVars +'">';
-	myYoutubePlaylist_cfStr += '<embed src="http://www.youtube.com/'+type+'/'+ movie +'&hl=en&fs=1" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="true"></embed>';
+	myYoutubePlaylist_cfStr += '<embed src="http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="true"></embed>';
+	myYoutubePlaylist_cfStr += '<![endif]-->';
 	myYoutubePlaylist_cfStr += '<' + '/object>';
 	return myYoutubePlaylist_cfStr;
 }
@@ -27,10 +29,11 @@ function myYoutubePlaylist_cf(FlashVars,movie,width,height,type) {
 //a function to load all movies with thumbs to a list
 function myYoutubePlaylist_dl(allVideos,containerId,targetContainerId) {
 	allVideosArr = allVideos.split(', ');
-	allVideosLi = '';
+	allVideosLi = '<ul class="myYoutubePlaylist_Ul" >';
 	for (var i=0; i < allVideosArr.length; i++) {
 		thisLink = 'javascript:myYoutubePlaylist_cy(\''+allVideosArr[i]+'\',\''+targetContainerId+'\');'
 		allVideosLi += '<li>' + '<a href="'+thisLink+'">' + '<img class="myYoutubePlaylist_Img"  alt="" src="http://i3.ytimg.com/vi/'+allVideosArr[i]+'/default.jpg"/>' + '</'+'a></'+'li>';
 	}
+	allVideosLi += '</ul>';
 	document.getElementById(containerId).innerHTML = allVideosLi;
 }
