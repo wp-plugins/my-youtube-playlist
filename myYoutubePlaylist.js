fa74f933ebb3799ce916ad799bec475efeb1d76f
jonkastonka <jonkastonka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-16 09:16:13 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/my-youtube-playlist/tags/1.2@241932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myYoutubePlaylist.js
+++ myYoutubePlaylist.js
@@ -11,21 +11,24 @@ if (document.all) {
 }
 
 //a function to choose another youtube-video without reloading the page
-function myYoutubePlaylist_cy(ytSrc,containerId,type) {
+function myYoutubePlaylist_cy(ytSrc,containerId,type,play) {
 	if (type == null || type == '') {
 		type = 'v';
 	}
-	document.getElementById(containerId).innerHTML = eval("myYoutubePlaylist_cf('',ytSrc,500,307,type)");
+	document.getElementById(containerId).innerHTML = eval("myYoutubePlaylist_cf('',ytSrc,500,307,type,play)");
 }
 
 //a function to load flashmovies. it also prevents ie from adding the ugly border
-function myYoutubePlaylist_cf(FlashVars,movie,width,height,type) {
-	myYoutubePlaylist_cfStr = '<obj' + 'ect width="' + width + '" height="' + height + '" data="' + 'http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1' + '" type="application/x-shockwave-flash">';
-	myYoutubePlaylist_cfStr += '<param name="movie" value="http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1"/>';
+function myYoutubePlaylist_cf(FlashVars,movie,width,height,type,play) {
+	if (play == 'play') {
+		play = '&autoplay=1';
+	}
+	myYoutubePlaylist_cfStr = '<obj' + 'ect width="' + width + '" height="' + height + '" data="' + 'http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1' + play + '" type="application/x-shockwave-flash">';
+	myYoutubePlaylist_cfStr += '<param name="movie" value="http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1' + play + '"/>';
 	myYoutubePlaylist_cfStr += '<param name="allowFullScreen" value="true"/>';
 	myYoutubePlaylist_cfStr += '<param name="allowscriptaccess" value="always"/>';
 	//myYoutubePlaylist_cfStr += '<param name="FlashVars" value="'+ FlashVars +'">';
-	myYoutubePlaylist_cfStr += '<embed src="http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="true"/>';
+	myYoutubePlaylist_cfStr += '<embed src="http://www.youtube.com/'+ type +'/'+ movie +'&hl=en&fs=1'+ play +'" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="true"/>';
 	myYoutubePlaylist_cfStr += '<' + '/object>';
 	return myYoutubePlaylist_cfStr;
 }
@@ -36,7 +39,7 @@ function myYoutubePlaylist_dl(allVideos,containerId,targetContainerId) {
 	if (allVideosArr.length > 1) {
 		allVideosLi = '<ul class="myYoutubePlaylist_Ul" >';	
 		for (var i=0; i < allVideosArr.length; i++) {
-			thisLink = 'javascript:myYoutubePlaylist_cy(\''+allVideosArr[i]+'\',\''+targetContainerId+'\');'
+			thisLink = 'javascript:myYoutubePlaylist_cy(\''+allVideosArr[i]+'\',\''+targetContainerId+'\',\'\',\''+'play'+'\');'
 			allVideosLi += '<li>' + '<a href="'+thisLink+'">' + '<img class="myYoutubePlaylist_Img"  alt="" src="http://i3.ytimg.com/vi/'+allVideosArr[i]+'/default.jpg"/>' + '</'+'a></'+'li>';
 		}
 		allVideosLi += '</ul>';
